dda7b90fe65666f57bd8f20b88b5f534155246e2
taovilla <taovilla90@gmail.com>
2023-05-13 14:37:59 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./build/*.html"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        productgreen:"#788579"
+      }
+    },
+  },
+  plugins: [],
+}
+
